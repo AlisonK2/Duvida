2ceae846097928f03b2594dc2c94c518002c5eb9
Alison A. Sales <68068625+AlisonK2@users.noreply.github.com>
2022-06-16 16:03:40 -0300

    update

--- Truco/client/Client.java
+++ Truco/client/Client.java
@@ -2,11 +2,14 @@ package client;
 
 // Importing libraries
 import util.Card;
+import util.Hand;
 import util.Player;
-import util.Requisicao;
+import util.Result;
+import util.Threads;
 import util.Computer;
 import java.net.Socket;
 import util.Comunicacao;
+import util.InitialData;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -14,22 +17,22 @@ import java.io.IOException;
 
 public class Client {
     // Instantiating static variables
-    static String msg_setting_pack;
-    static String msg_setting_match;
+    static int game_mode;
+    static int pack_type;
+    static Double bot_difficulty;
+    static Comunicacao comunicacao;
     static Random random = new Random();
     static Player player = new Player();
-    static String msg_setting_difficulty;
+    static Computer computer = new Computer();
     static Scanner sc = new Scanner(System.in);
 
     // ------------------------ Main ------------------------
     public static void main(String[] args) {
         // Instantiating variables
-        Socket socket = null;
-        Scanner input = null;
+        Socket socket;
+        Scanner input;
         final int PORT = 1234;
         final String IP = "127.0.0.1";
-        Comunicacao comunicacao;
-        Requisicao requisicao;
 
         // Solicitar conexão
         try {
@@ -44,13 +47,28 @@ public class Client {
         // Comunicação
         try {
             input = new Scanner(socket.getInputStream());
-            // output = new PrintStream(socket.getOutputStream());
             Listen listen = new Listen(input);
             listen.start();
 
-            setting_match(comunicacao);
-            start_match(comunicacao);
+            setting_name();
 
+            // Se o tamanho da Thread for 1, ou seja, outro jogardor ainda não configurou 
+            // o jogo, então o primeiro jogador a se conectar configurarar o jogo
+            comunicacao.enviar(new Threads()); 
+            ArrayList<Card> threads = (ArrayList<Card>) comunicacao.receber();
+            if (threads.size() == 1){ 
+                setting_match();
+                setting_pack();
+
+                if (game_mode == 1) {
+                    setting_difficulty();
+                }
+                
+                comunicacao.enviar(new InitialData(player.getName(), game_mode, pack_type, bot_difficulty));
+            }
+            
+            start_match();
+            
         } catch (Exception e) {
             System.out.println(" >>> Erro na comunicação");
             System.out.println(e.getMessage());
@@ -58,41 +76,36 @@ public class Client {
 
         // Encerrar conexão
         try {
-            input.close();
-            // output.close();
+            // input.close();
             socket.close();
         } catch (Exception e) {
-
             System.out.println(e.getMessage());
         }
     }
 
     // ------------------------ Methods ------------------------
-    public static void setting_match(Comunicacao comunicacao) {
+    public static void setting_name() {
         System.out.println("\n >>> Digite o seu nome:");
         System.out.print(" --> ");
-        msg_setting_match = sc.nextLine();
-
-        player.setName(msg_setting_match);
-        Requisicao requisicao = new Requisicao(14, player);
-        comunicacao.enviar(requisicao); // Envia a variável "player" para ser armazenada no Match
+        player.setName(sc.nextLine());
 
         try {
-        clear_console();
+            clear_console();
         } catch (InterruptedException | IOException e) {
-        System.out.println("\n\n >>> Erro inesperado");
-        System.out.println(e.getMessage());
+            System.out.println("\n\n >>> Erro inesperado\n");
+            System.out.println(e.getMessage());
         }
+    }
 
+    public static void setting_match() {
         do {
             System.out.println("\n Bem-vindo/a, " + player.getName() + ", ao jogo de TRUCO\n"
                     + " 1 - Modo de jogo solo\n"
                     + " 2 - Aguardar mais jogadores\n"
-                    + " 3 - Sair\n"
                     + "\n >>> Escolha uma opção:");
 
             System.out.print(" --> ");
-            msg_setting_match = sc.nextLine();
+            game_mode = sc.nextInt();
 
             try {
                 clear_console();
@@ -101,40 +114,27 @@ public class Client {
                 System.out.println(e.getMessage());
             }
 
-            switch (msg_setting_match) {
-                case "1":
-                    requisicao = new Requisicao(1, 0);
-                    comunicacao.enviar(requisicao); // Requisita método "setGame_mode()"
-                    setting_pack(comunicacao);
+            switch (game_mode) {
+                case 1:
+                case 2:
                     break;
-
-                case "2":
-                    break;
-
-                case "3":
-                    System.out.println("\n\n >>> Volte sempre!"
-                            + "\n >>> Desconectado\n\n\n");
-                    break;
-
                 default:
-                    System.out.println("\n >>> Por favor, digite uma opção válida\n");
+                    game_mode = 0;
+                    System.out.println("\n\n >>> Por favor, digite uma opção válida\n\n");
                     break;
             }
-        } while (!msg_setting_match.equals("3"));
+        } while (game_mode == 0);
     }
 
-    public static void setting_pack(Comunicacao comunicacao) {
-        Requisicao requisicao;
-
+    public static void setting_pack() {
         do {
             System.out.println("\n Tipos de baralhos: \n"
                     + " 1 - Limpo (3, 2, A, K, J, Q)\n"
                     + " 2 - Sujo  (3, 2, A, K, J, Q, 7, 6, 5, 4)\n"
-                    + " 3 - Voltar ao menu anterior\n"
                     + "\n >>> Deseja jogar com qual baralho: ");
 
             System.out.print(" --> ");
-            msg_setting_pack = sc.nextLine();
+            pack_type = sc.nextInt();
 
             try {
                 clear_console();
@@ -143,49 +143,29 @@ public class Client {
                 System.out.println(e.getMessage());
             }
 
-            switch (msg_setting_pack) {
-                case "1":
-                    requisicao = new Requisicao(2);
-                    comunicacao.enviar(requisicao); // Requisita método "setMatch_clean_pack()"
-
-                    requisicao = new Requisicao(4);
-                    comunicacao.enviar(requisicao); // Requisita método "setMatch_clean_pack()"
-                    setting_difficulty(comunicacao);
+            switch (pack_type) {
+                case 1:
+                case 2:
                     break;
-
-                case "2":
-                    requisicao = new Requisicao(3);
-                    comunicacao.enviar(requisicao); // Requisita método "setMatch_dirty_pack()"
-
-                    requisicao = new Requisicao(4);
-                    comunicacao.enviar(requisicao); // Requisita método "setMatch_clean_pack()"
-                    setting_difficulty(comunicacao);
-                    break;
-
-                case "3":
-                    break;
-
                 default:
+                    pack_type = 0;
                     System.out.println("\n >>> Por favor, digite uma opção válida\n");
                     break;
             }
-        } while (!msg_setting_pack.equals("3"));
+        } while (pack_type == 0);
     }
 
-    public static void setting_difficulty(Comunicacao comunicacao) {
-        Requisicao requisicao;
-
+    public static void setting_difficulty() {
         do {
             System.out.println("\n Dificuldades: \n"
                     + " 1 - Fácil\n"
                     + " 2 - Normal\n"
                     + " 3 - Díficil\n"
                     + " 4 - Profissional\n"
-                    + " 5 - Voltar ao menu anterior\n"
                     + "\n >>> Em qual nível deseja jogar: ");
 
             System.out.print(" --> ");
-            msg_setting_difficulty = sc.nextLine();
+            bot_difficulty = sc.nextDouble();
 
             try {
                 clear_console();
@@ -194,111 +174,71 @@ public class Client {
                 System.out.println(e.getMessage());
             }
 
-            switch (msg_setting_difficulty) {
-                case "1":
-                    requisicao = new Requisicao(5, 0.4);
-                    comunicacao.enviar(requisicao); // Requisita método "setDifficulty()"
-                    msg_setting_difficulty = "5";
-                    msg_setting_pack = "3";
-                    msg_setting_match = "3";
+            switch (bot_difficulty.intValue()) {
+                case 1:
+                    bot_difficulty = 0.10;
                     break;
 
-                case "2":
-                    requisicao = new Requisicao(5, 0.55);
-                    comunicacao.enviar(requisicao); // Requisita método "setDifficulty()"
-                    msg_setting_difficulty = "5";
-                    msg_setting_pack = "3";
-                    msg_setting_match = "3";
+                case 2:
+                    bot_difficulty = 0.20;
                     break;
 
-                case "3":
-                    requisicao = new Requisicao(5, 0.75);
-                    comunicacao.enviar(requisicao); // Requisita método "setDifficulty()"
-                    msg_setting_difficulty = "5";
-                    msg_setting_pack = "3";
-                    msg_setting_match = "3";
+                case 3:
+                    bot_difficulty = 0.50;
                     break;
 
-                case "4":
-                    requisicao = new Requisicao(5, 0.9);
-                    comunicacao.enviar(requisicao); // Requisita método "setDifficulty()"
-                    msg_setting_difficulty = "5";
-                    msg_setting_pack = "3";
-                    msg_setting_match = "3";
+                case 4:
+                    bot_difficulty = 0.60;                
                     break;
-
-                case "5":
-                    break;
-
                 default:
+                    bot_difficulty = 0.0;
                     System.out.println("\n >>> Por favor, digite uma opção válida\n");
                     break;
             }
-        } while (!msg_setting_difficulty.equals("5"));
+        } while (bot_difficulty == 0.0);
     }
 
-    private static void start_match(Comunicacao comunicacao) {
-        Requisicao requisicao;
+    private static void start_match() {
         Computer pc = new Computer();
 
         do { // Partida até 12
-            int j = 0;
+            int mao = 0;
             int msg = 1;
-            Card mesa = null;
-            Card manilha = null;
+            Card mesa = new Card();
+            Card manilha = new Card();
             Card used_card = new Card();
             ArrayList<Card> used_cards = new ArrayList<>();
-            ArrayList<Card> player_hand = new ArrayList<>();
-            ArrayList<Card> computer_hand = new ArrayList<>();
 
-            do { // Rodada, melhor de 3
-                requisicao = new Requisicao(10);
-                comunicacao.enviar(requisicao); // Requisita método "embaralhar()"
+            do {  // Rodada, possui 3 mãos ou até alguém completar 2 pontos
+                 // Envia requisição para: Emabaralhar, dar as cartas, dizer a carta 
+                // que vira na mesa e consequentemente definir a manilha
+                comunicacao.enviar(new Hand());
+                Hand hand = (Hand) comunicacao.receber();
 
-                requisicao = new Requisicao(11, player_hand, computer_hand);
-                comunicacao.enviar(requisicao); // Requisita método "dar_cartas()"
-
-                requisicao = new Requisicao(15);
-                comunicacao.enviar(requisicao); // Requisita método "getPlayer().getHand()"
-                player_hand = (ArrayList<Card>) comunicacao.receber().getValor();
-
-                requisicao = new Requisicao(16);
-                comunicacao.enviar(requisicao); // Requisita método "getPc().getHand()"
-                computer_hand = (ArrayList<Card>) comunicacao.receber().getValor();
-
-                requisicao = new Requisicao(8);
-                comunicacao.enviar(requisicao); // Requisita método "getRound_pack()"
-                mesa = (Card) comunicacao.receber().getValor(); // Recebe variável "Round_pack"
-
-                requisicao = new Requisicao(7, mesa);
-                comunicacao.enviar(requisicao); // Requisita método "setting_manilha()"
-                manilha = (Card) comunicacao.receber().getValor(); // Recebe variável "manilha"
+                // Váriaveis recebidas do servidor sendo configuradas do lado do Client 
+                mesa = hand.getMesa();
+                manilha = hand.getManilha();
+                player.setHand(hand.getHand()); 
 
                 System.out.println("\n >>> " + player.getName() + " vs Computador"
-                        + "\n >>> Virou um " + mesa.getSymbol() + " na mesa.");
+                                 + "\n >>> Virou um " + mesa.getSymbol() + " na mesa.");
 
-                do {
-                    do {
-                        if (msg > 4 || msg == 0) { // verificar isso aqui mano
-                            System.out.println("\nPor favor, digite uma opção válida.\n");
-                        }
+                do { // Loop para garantir que o jogador não jogará uma carta que já usou ou uma opção inválida
+                    if (msg > 4 || msg == 0) { 
+                        System.out.println("\nPor favor, digite uma opção válida.\n");
+                    }
+                    
+                    System.out.println("\n >>> A manilha é " + manilha.getSymbol()
+                                     + "\n >>> Sua mão:"
+                                     + "\n 1 - " + player.getHand().get(0).getSymbol() + " " + player.getHand().get(0).getSuit().getName()
+                                     + "\n 2 - " + player.getHand().get(1).getSymbol() + " " + player.getHand().get(1).getSuit().getName()
+                                     + "\n 3 - " + player.getHand().get(2).getSymbol() + " " + player.getHand().get(2).getSuit().getName()
+                                     + "\n 4 - Pedir Truco\n"
+                                     + "\n >>> Escolha uma opção: ");
+                    System.out.print(" --> ");
+                    msg = sc.nextInt();
 
-                        System.out.println("\n >>> A manilha é " + manilha.getSymbol()
-                                + "\n >>> Sua mão:"
-                                + "\n 1 - " + player_hand.get(0).getSymbol() + " "
-                                + player_hand.get(0).getSuit().getName()
-                                + "\n 2 - " + player_hand.get(1).getSymbol() + " "
-                                + player_hand.get(1).getSuit().getName()
-                                + "\n 3 - " + player_hand.get(2).getSymbol() + " "
-                                + player_hand.get(2).getSuit().getName()
-                                + "\n 4 - Pedir Truco\n"
-                                + "\n >>> Escolha uma opção: ");
-                        System.out.print(" --> ");
-                        msg = sc.nextInt();
-
-                    } while (msg > 4);
-
-                    used_card = player_hand.get(msg - 1);
+                    used_card = player.getHand().get(msg - 1);
 
                     if (used_cards.contains(used_card)) {
                         try {
@@ -318,8 +258,11 @@ public class Client {
                     case 3:
                         used_cards.add(used_card);
                         player.setUsed_card(used_card);
-                        requisicao = new Requisicao(12, player.getUsed_card());
-                        comunicacao.enviar(requisicao); // Requisita método "pc_play()"
+
+                        // -----------------------------------------------------------
+                        comunicacao.enviar(new Result(player, computer, 0));
+                        Result result = (Result) comunicacao.receber();
+                        pc = result.getComputer();
 
                         try {
                             clear_console();
@@ -327,20 +270,13 @@ public class Client {
                             System.out.println("\n\n >>> Erro inesperado");
                             System.out.println(e.getMessage());
                         }
-                        
-                        requisicao = new Requisicao(9);
-                        comunicacao.enviar(requisicao); // Requisita variável "pc"
-                        pc = (Computer) comunicacao.receber().getValor(); // Recebe variável "pc"
 
                         System.out.println("\n\n >>> " + player.getName() + " jogou " + used_card.getSymbol() + " "
                                 + used_card.getSuit().getName());
                         System.out.println(" >>> Computador jogou " + pc.getUsed_card().getSymbol() + " "
                                 + pc.getUsed_card().getSuit().getName());
 
-                        requisicao = new Requisicao(13, player.getUsed_card(), pc.getUsed_card(), manilha);
-                        comunicacao.enviar(requisicao);
-                        int h = (int) comunicacao.receber().getValor();
-                        if (h == 1) {
+                        if (result.getWinner() == 0) {
                             player.setRound_score(player.getRound_score() + 1);
                             System.out.println("\n\n\n >>> " + player.getName() + " levou essa !"
                                     + "\n >>> Pontos da rodadas: " + player.getRound_score()
@@ -360,9 +296,7 @@ public class Client {
                         break;
 
                     case 4:
-
                         break;
-
                     default:
                         System.out.println("\n >>> Por favor, digite uma opção válida\n");
                         break;
@@ -384,14 +318,14 @@ public class Client {
                             + "\n >>> Pontos da partida:" + player.getMatch_score() + "\n");
                 }
 
-                j++;
-            } while (j < 2 || (player.getRound_score() < 2 && pc.getRound_score() < 2));
+                mao++;
+            } while (mao < 2 || (player.getRound_score() < 2 && pc.getRound_score() < 2));
 
             // Limpando a mão para o próximo round --> Partida (vai até 12 pontos) -->
             // Possui no máximo 12 round ao todo --> Dentro de cada round tem 3 outros
             // rounds
-            player_hand.clear();
-            computer_hand.clear();
+            player.getHand().clear();
+            computer.getHand().clear();
             used_cards.clear();
             player.setRound_score(0);
             pc.setRound_score(0);
